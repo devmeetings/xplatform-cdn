7d7c9545977469f059377603fe5feee980b0685d
Tomusdrw <tomusdrw@gmail.com>
2015-09-09 15:01:37 +0200

    Adding assets generator for rwd shop

---
+++ rwd-shop/1.0.0/generate.js
@@ -0,0 +1,59 @@
+var glob = require('glob');
+var path = require('path');
+var Q = require('q');
+
+var im = require('imagemagick');
+
+var cwd = 'src';
+var assets = 'assets';
+
+var baseUrl = 'https://local.xplatform.org/cdn/rwd-shop/1.0.0/';
+
+var htmlStart = ['<html><head><title>Assets</title>',
+'<link rel="stylesheet" href="styles.css">',
+'</head><body><div class="container">'].join('\n');
+var htmlEnd = '</div></body></html>';
+
+var buildAsset = function (asset) {
+  return [
+    '<div class="asset">',
+    '<img src="' + asset.path + '">',
+    '<label>1x</label>',
+    '<input type="text" readonly value="' + asset.url + '">',
+    '<label>2x</label>',
+    '<input type="text" readonly value="' + asset.url2x + '">',
+    '</div>'
+  ].join('\n');
+};
+
+glob(path.join(cwd, '*.jpg'), function (err, files) {
+  if (err) {
+    throw err;
+  }
+  console.log(htmlStart);
+  Q.all(
+    files.map(function (file) {
+      var ext = path.extname(file);
+      var name = path.basename(file, ext);
+
+      var baseFileName = path.join(assets, name + ext);
+      var baseFileName2x = path.join(assets, name + '_2x' + ext);
+      return Q.all([
+        Q.ninvoke(im, 'convert', [file, '-quality', '90', '-resize', '250x', baseFileName]),
+        Q.ninvoke(im, 'convert', [file, '-quality', '90', '-resize', '500x', baseFileName2x])
+      ]).then(function (all) {
+        return {
+          path: baseFileName,
+          url: baseUrl + baseFileName,
+          url2x: baseUrl + baseFileName2x
+        };
+      });
+    })
+  ).done(function (assets) {
+    assets.map(function (asset) {
+      console.log(buildAsset(asset));
+    });
+    console.log(htmlEnd);
+  });
+
+});
